3f543edb6a7a299c97df17723a207a7cc77f0543
Roshni <roshnisahu11222@gmail.com>
2026-06-18 01:07:38 +0530

    Added While loop concepts

---
+++ 03_Control_flow/8_whileLoop.js
@@ -0,0 +1,19 @@
+// while loop
+
+let i = 1
+while(i <= 10)
+{
+    console.log(`i = ${i}`);
+    i++
+    // i = i + 2
+}
+
+
+// Array
+let myArray = ["rose", "sunflower", "lotus"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
