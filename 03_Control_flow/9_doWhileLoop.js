6325982192b68765f38ec7794184bae4ec9d22c4
Roshni <roshnisahu11222@gmail.com>
2026-06-18 01:08:13 +0530

    Added do-While loop concepts

---
+++ 03_Control_flow/9_doWhileLoop.js
@@ -0,0 +1,18 @@
+// do - while loop
+
+let j = 1
+do {
+    // console.log( j*2);
+    console.log(`2 * ${j} = ${j*2}`);
+    j++
+
+} while (j <= 10);
+
+console.log("------------------------->>>");
+
+let k = 11   // first execute then condition checks
+do {
+    console.log(`value is ${k}`);
+    k++
+    
+} while (k <= 10);
